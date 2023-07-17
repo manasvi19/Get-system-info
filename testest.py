7fa62bfd6400fb5a91c4a54f6983118f0aaef65c
manasvi19 <84223806+manasvi19@users.noreply.github.com>
2023-07-17 15:01:18 +0530

    Update testest.py

--- testest.py
+++ testest.py
@@ -69,8 +69,8 @@ def get_system_info():
     system_info['Memory'] = memory_info
     
     # Get disk information
-    disk_info = run_command('diskutil list')
-    system_info['Disk Information'] = disk_info
+    #disk_info = run_command('diskutil list')
+    #system_info['Disk Information'] = disk_info
     
     # Get disk usage information
     disk_usage = get_disk_usage()
