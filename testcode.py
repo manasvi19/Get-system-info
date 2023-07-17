a76ac6bfaf04a20c751f84ba4beb45b12211e7dd
manasvi19 <84223806+manasvi19@users.noreply.github.com>
2023-07-17 13:38:56 +0530

    Update testcode.py

--- testcode.py
+++ testcode.py
@@ -50,7 +50,13 @@ def get_system_info():
     system_info['IP Addresses'] = ip_addresses
     network_cards_output = run_command('networksetup -listallhardwareports')
     network_cards = network_cards_output.split('\n')
-    system_info['Network Cards'] = network_cards
+    formatted_network_cards = '\n'.join(network_cards)
+    system_info['Network Cards'] = formatted_network_cards
+
+    
+    #network_cards_output = run_command('networksetup -listallhardwareports')
+    #network_cards = network_cards_output.split('\n')
+    #system_info['Network Cards'] = network_cards
     
     
     
