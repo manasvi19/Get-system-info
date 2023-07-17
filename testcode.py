0fdf272972b07b5c17f44c4b216d2d6168abb99c
manasvi19 <84223806+manasvi19@users.noreply.github.com>
2023-07-17 13:25:13 +0530

    Update testcode.py

--- testcode.py
+++ testcode.py
@@ -1,15 +1,65 @@
 #!/usr/bin/env python
 # coding: utf-8
-def process = "systeminfo".execute()
-def output = process.text
-def lines = output.split("\n")
-def formattedInfo = []
-lines.each { line ->
-            formattedInfo.add(line[2..-2])
-            }
 
-formattedInfo.each { info ->
-println info
-                   }
+import platform
+import subprocess
 
 
+# Function to run a shell command and return the output
+def run_command(command):
+    process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+    output, _ = process.communicate()
+    return output.decode('utf-8').strip()
+
+# Get system information
+def get_system_info():
+    system_info = {}
+    
+    # Get host name
+    host_name = run_command('scutil --get ComputerName')
+    system_info['Host Name'] = host_name
+    
+    # Get operating system information
+    os_name = platform.system()
+    os_version = platform.mac_ver()[0]
+    os_build = run_command('sw_vers -buildVersion')
+    #os_manufacturer = "Apple"
+    os_manufacturer = platform.system()
+    print("OS Manufacturer:", os_manufacturer)
+    system_info['OS Name'] = os_name
+    system_info['OS Version'] = os_version
+    system_info['OS Build'] = os_build
+    system_info['OS Manufacturer'] = os_manufacturer
+    
+    # Get processor information
+    processor_name = run_command('sysctl -n machdep.cpu.brand_string')
+    system_info['Processor'] = processor_name
+    
+    # Get memory information
+    memory_info = run_command('sysctl -n hw.memsize')
+    system_info['Memory'] = memory_info
+    
+    # Get disk information
+    disk_info = run_command('diskutil list')
+    system_info['Disk Information'] = disk_info
+    
+    # Get network interfaces
+    network_interfaces = run_command('ifconfig -a')
+    system_info['Network Interfaces'] = network_interfaces
+    ip_addresses_output = run_command('ifconfig | grep "inet " | awk \'{print $2}\'')
+    ip_addresses = ip_addresses_output.split('\n')
+    system_info['IP Addresses'] = ip_addresses
+    
+    # Get graphics information
+    graphics_info = run_command('system_profiler SPDisplaysDataType')
+    system_info['Graphics Information'] = graphics_info
+
+    
+    return system_info
+
+# Get and print system information
+system_info = get_system_info()
+for key, value in system_info.items():
+    print(f'{key}:')
+    print(value)
+    print('-' * 50)
